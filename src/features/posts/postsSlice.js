8c4636a1989f3aab0d670018137d843a2e3c7080
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-09-29 00:42:57 +0200

    Implement Reaction Buttons for Posts
    Added new compoent ReactionButtons and new property reactions in postSlice. Now, you can interact with posts by liking etc.

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,10 +2,30 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from 'date-fns'
 
 const initialState = [
-    { id: '1', title: 'Football', content: 'Footbal is a sport and very popular', date: sub(new Date(), { minutes: 10 }).toISOString() },
-    { id: '2', title: 'Movies', content: 'These movies are so good!', date: sub(new Date(), { minutes: 5 }).toISOString() },
-    { id: '3', title: 'IMDB', content: 'I like to check the ratings of the movies I watched on IMDB', date: sub(new Date(), { minutes: 12 }).toISOString() },
-    { id: '4', title: 'News are good?', content: 'What do you mean, if the news are good?', date: sub(new Date(), { minutes: 20 }).toISOString() }
+    {   id: '1', 
+        title: 'Football', 
+        content: 'Footbal is a sport and very popular', 
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
+    },
+    {   id: '2', 
+        title: 'Movies', 
+        content: 'These movies are so good!', 
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        } 
+    }
 ]
 
 const postsSlice = createSlice({
@@ -15,9 +35,21 @@ const postsSlice = createSlice({
         postAdded: {
             reducer(state, action) { state.push(action.payload) }, // state.push normally would mutate the state. but it does only not hier inside the createSlice
             prepare(title, content, userId) { 
-                return { payload: { id: nanoid(), title, content, date: new Date().toISOString(), userId } 
-            } }
-        }    
+                return { payload: { id: nanoid(),
+                                    title, 
+                                    content, 
+                                    date: new Date().toISOString(), 
+                                    userId, 
+                                    reactions: { thumbsUp: 0, wow: 0, heart: 0, rocket: 0, coffee: 0} 
+                                } 
+                        } 
+                }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) existingPost.reactions[reaction]++ // works only in createSlice, would normally mutate the state
+        }
     }
 })
 
@@ -25,7 +57,7 @@ const postsSlice = createSlice({
 export const getAllPosts = (state) => state.posts
 
 // Actions
-export const { postAdded } = postsSlice.actions // when we add postAdded function in reducers, than crateSlice automatically generates an action creater function with the same name.
+export const { postAdded, reactionAdded } = postsSlice.actions // when we add postAdded function in reducers, than crateSlice automatically generates an action creater function with the same name.
 
 // Reducer
 export default postsSlice.reducer
