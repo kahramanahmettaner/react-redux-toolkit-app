baee6cdebd853418dbda94040190780b088493a0
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-10-21 00:50:09 +0200

    Add post data using async thunk

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import usersSlice, { getAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
@@ -10,6 +10,7 @@ const AddPostForm = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const [userId, setUserId] = useState('')
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
     const users = useSelector(getAllUsers)
 
@@ -17,14 +18,23 @@ const AddPostForm = () => {
     const onContentChanged = e => setContent(e.target.value)
     const onAuthorChanged = e => setUserId(e.target.value)
     
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
 
     const onSavePostClicked = () => {
-        if (!canSave) return
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, body: content, userId })).unwrap() // 'unwrap()' handles the promise, returning the payload or throwing an error if rejected.
 
-        dispatch(postAdded(title, content, userId)) 
-        setTitle('')
-        setContent('')
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.error('Failed to save the post', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
+        }
     }
 
     const usersOptions = users.map(user => (
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -15,6 +15,10 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     return response.data
 })
 
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost)
+    return response.data 
+})
 
 const postsSlice = createSlice({
     name: 'posts',
@@ -28,7 +32,7 @@ const postsSlice = createSlice({
                                     content, 
                                     date: new Date().toISOString(), 
                                     userId, 
-                                    reactions: { thumbsUp: 0, wow: 0, heart: 0, rocket: 0, coffee: 0} 
+                                    reactions: { thumbsUp: 0, wow: 0, heart: 0, rocket: 0, coffee: 0 } 
                                 } 
                         } 
                 }
@@ -59,7 +63,7 @@ const postsSlice = createSlice({
                         hooray: 0,
                         heart: 0,
                         rocket: 0,
-                        eyes: 0
+                        coffee: 0
                     }
                     post.id = nanoid()
                     return post;
@@ -68,7 +72,19 @@ const postsSlice = createSlice({
                 // Add any fetched posts to the array
                 state.posts = state.posts.concat(loadedPosts) // concat: only inside createSlice
             })
-
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                action.payload.userId = Number(action.payload.userId)
+                action.payload.date = new Date().toISOString()
+                action.payload.reactions = {
+                    thumbsUp: 0,
+                    hooray: 0,
+                    heart: 0,
+                    rocket: 0,
+                    coffee: 0
+                }
+                console.log(action.payload)
+                state.posts.push(action.payload)
+            })
 
 
     }
