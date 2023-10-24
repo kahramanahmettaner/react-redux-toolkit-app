a9009b7ceeb1e53d8da44dd51297a34434e723cd
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-10-24 22:34:38 +0200

    Implement post editing functionality
    - Introduce a new component 'EditPostForm.js' for post editing.
    
    - Add a new route 'editpost' to enable editing posts.
    
    - Implement the 'updatePost' async thunk in 'postSlice.js' to update posts.
    
    - Add an extra reducer in 'postSlice' to handle 'updatePost.fulfilled' actions.

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -4,6 +4,7 @@ import ReactionButtons from "./ReactionButtons"
 import { useSelector } from "react-redux"
 import { getPostById } from "./postsSlice"
 import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const SinglePostPage = () => {
     const { postId } = useParams()
@@ -23,6 +24,7 @@ const SinglePostPage = () => {
             <h3>{post.title}</h3>
             <p>{post.body}</p>
             <p className="postCredit">
+                <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date}/>
             </p>
